4e9b1693c9a4845b33d48dd28e4a4ab4cb3d6979
Tanguy Pinel <tanguypinel@gmail.com>
2024-09-28 17:00:28 +0200

    Update marquee block styling and functionality, and add acc-imprimerie block

--- acf-blocks/acc-imprimerie/view.php
+++ acf-blocks/acc-imprimerie/view.php
@@ -3,4 +3,15 @@
     extract($fields);
     ?>
     <section class="imprimerie">
+        <div class="left">
+            <h1 data-aos="zoom-in" data-aos-delay="300" ><?php echo $titre_bleu; ?></h1>
+            <p data-aos="zoom-in" data-aos-delay="300" ><?php echo $sous_titre_noir; ?></p>
+            <h2 data-aos="zoom-in" data-aos-delay="600" ><?php echo $baseline; ?></h2>
+        </div>
+        <div class="right">
+            <span data-aos="fade-in" data-aos-delay="800" class="left"><?php echo wp_get_attachment_image($pierre, 'full') ?></span>
+            <span data-aos="fade-right" data-aos-delay="300" class="center"><?php echo wp_get_attachment_image($gif,'full') ?></span>
+            <span data-aos="fade-in" data-aos-delay="800" class="right"><?php echo wp_get_attachment_image($pm,'full') ?></span>
+
+        </div>
     </section>
