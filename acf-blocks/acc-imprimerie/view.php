ecce910c9abdc27d141ef2a184b508a4ca776ac8
Tanguy Pinel <tanguypinel@gmail.com>
2024-10-09 11:19:00 +0200

    Wow Responsive

--- acf-blocks/acc-imprimerie/view.php
+++ acf-blocks/acc-imprimerie/view.php
@@ -9,9 +9,16 @@
             <h2 data-aos="zoom-in" data-aos-delay="600" ><?php echo $baseline; ?></h2>
         </div>
         <div class="right">
-            <span data-aos="fade-in" data-aos-delay="800" class="left"><?php echo wp_get_attachment_image($pierre, 'full') ?></span>
-            <span data-aos="fade-right" data-aos-delay="300" class="center"><?php echo wp_get_attachment_image($gif,'full') ?></span>
-            <span data-aos="fade-in" data-aos-delay="800" class="right"><?php echo wp_get_attachment_image($pm,'full') ?></span>
+            <span data-aos="fade-in" data-aos-offset="0" data-aos-delay="400" class="left"><?php echo wp_get_attachment_image($pierre, 'full') ?></span>
+            <span data-aos="fade-right" data-aos-offset="0" data-aos-delay="300" class="center"><?php echo wp_get_attachment_image($gif,'full') ?></span>
+            <span data-aos="fade-in" data-aos-offset="0" data-aos-delay="400" class="right"><?php echo wp_get_attachment_image($pm,'full') ?></span>
 
         </div>
+
+        <!-- <div class="video-container" data-aos="fade-up" data-aos-delay="1000">
+            <video loop autoplay muted>
+                <source src="http://localhost:3000/wp-content/themes/kyodai/dist/image/video.webm" type="video/webm">
+                Your browser does not support the video tag.
+            </video>
+        </div> -->
     </section>
