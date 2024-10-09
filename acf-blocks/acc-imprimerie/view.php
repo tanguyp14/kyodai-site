7ff7766c1723ad7f1e468dd53dbc345482808650
Tanguy Pinel <tanguypinel@gmail.com>
2024-10-09 15:24:13 +0200

    good

--- acf-blocks/acc-imprimerie/view.php
+++ acf-blocks/acc-imprimerie/view.php
@@ -4,14 +4,14 @@
     ?>
     <section class="imprimerie">
         <div class="left">
-            <h1 data-aos="zoom-in" data-aos-delay="300" ><?php echo $titre_bleu; ?></h1>
-            <p data-aos="zoom-in" data-aos-delay="300" ><?php echo $sous_titre_noir; ?></p>
-            <h2 data-aos="zoom-in" data-aos-delay="600" ><?php echo $baseline; ?></h2>
+            <h1 data-aos="zoom-in" data-aos-delay="300"><?php echo $titre_bleu; ?></h1>
+            <p data-aos="zoom-in" data-aos-delay="300"><?php echo $sous_titre_noir; ?></p>
+            <h2 data-aos="zoom-in" data-aos-delay="600"><?php echo $baseline; ?></h2>
         </div>
         <div class="right">
-            <span data-aos="fade-in" data-aos-offset="0" data-aos-delay="400" class="left"><?php echo wp_get_attachment_image($pierre, 'full') ?></span>
-            <span data-aos="fade-right" data-aos-offset="0" data-aos-delay="300" class="center"><?php echo wp_get_attachment_image($gif,'full') ?></span>
-            <span data-aos="fade-in" data-aos-offset="0" data-aos-delay="400" class="right"><?php echo wp_get_attachment_image($pm,'full') ?></span>
+            <span data-aos="fade-in" data-aos-delay="600" class="left"><?php echo wp_get_attachment_image($pierre, 'full') ?></span>
+            <span data-aos="fade-right" data-aos-delay="400" class="center"><?php echo wp_get_attachment_image($gif, 'full') ?></span>
+            <span data-aos="fade-in" data-aos-delay="600" class="right"><?php echo wp_get_attachment_image($pm, 'full') ?></span>
 
         </div>
 
