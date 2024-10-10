948609f8bd431f10172a868d186a6cae1f585baa
Tanguy Pinel <tanguypinel@gmail.com>
2024-10-10 11:36:23 +0200

    Update styling and functionality of acc-pictos block

--- acf-blocks/acc-pictos/view.php
+++ acf-blocks/acc-pictos/view.php
@@ -1,14 +1,16 @@
     <?php
     $fields = get_fields();
     extract($fields);
+    $t = 3;
     ?>
     <section class="pictos_container">
         <div class="pictos">
             <?php foreach ($pictos as $picto) : extract($picto); ?>
-                <div class="picto">
-                    <span class="icon" style="background-image: url('<?php echo wp_get_attachment_url($image); ?>')"></span>
+                <div class="picto" data-aos="<?= wp_is_mobile() ? 'fade-right' : 'fade-up' ?>" data-aos-delay="<?= $t ?>00">
+                    <span class="icon" style="background-image: url('<?php echo wp_get_attachment_url($image); ?>'); animation-duration: <?= $t ?>s"></span>
                     <span class="text"><?php echo $texte; ?></span>
                 </div>
-            <?php endforeach; ?>
+
+            <?php $t++; endforeach;  ?>
         </div>
     </section>
