4a41519361e7511da897b8807af3ff22d872542a
Tanguy Pinel <tanguypinel@gmail.com>
2024-10-11 09:59:52 +0200

    Update breakpoints and styling of acc-avis block

--- acf-blocks/acc-avis/assets/js/view.js
+++ acf-blocks/acc-avis/assets/js/view.js
@@ -1,3 +1,27 @@
-(function($) {
+(function ($) {
+	$(document).ready(function () {
+		$('.avis').slick({
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			centerMode: true,
+			arrows: true,
+			variableWidth: true,
+			prevArrow: $('.arrow_left'),
+			nextArrow: $('.arrow_right'),
+		});
 
+		// Cibler uniquement les éléments originaux
+		$('.avis .avi').each(function () {
+			var top = Math.floor(Math.random() * 41) - 20;
+			var rotate = Math.floor(Math.random() * 21) - 10;
+			var className = $(this).attr('data-id'); // Récupérer la classe de l'élément
+
+			// Appliquer les styles à tous les éléments ayant la même classe
+			$('.avis .avi[data-id="' + className + '"]').css({
+
+			});
+		});
+
+	});
 })(jQuery);
--- acf-blocks/acc-avis/view.php
+++ acf-blocks/acc-avis/view.php
@@ -1,12 +1,20 @@
     <?php
     $fields = get_fields();
     extract($fields);
+    $d = 1;
     ?>
-    <section class="avis">
-    <?php foreach ($avis as $avi) : extract($avi); ?>
-        <div class="avi">
-            <p class="name"><?php echo $nom; ?></p>
-            <p class="text"><?php echo $zone_de_texte; ?></p>
+    <section class="avis_container">
+        <span class="arrow arrow_left"></span>
+        <div class="avis">
+
+            <?php foreach ($avis as $avi) : extract($avi); ?>
+                <div class="avi" data-id="<?php echo $d; ?>">
+                    <span class="icon" style="background-image: url('<?php echo wp_get_attachment_url($icon); ?>');"></span>
+                    <p class="name"><?php echo $nom; ?></p>
+                    <p class="text"><?php echo $zone_de_texte; ?></p>
+                </div>
+            <?php $d++;
+            endforeach; ?>
         </div>
-        <?php endforeach; ?>
+        <span class="arrow arrow_right"></span>
     </section>
