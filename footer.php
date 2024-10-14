7eb55525df469f78233732a102391def721d0b5f
Tanguy Pinel <tanguypinel@gmail.com>
2024-10-14 12:41:57 +0200

    ready home page

--- footer.php
+++ footer.php
@@ -1,17 +1,25 @@
 		<a href="#page" class="back-top">↑<span>Retour en haut</span></a>
-	</main><!-- /#main-content -->
-<footer role="contentinfo">
-	<div class="bottom-bar">
-		<p>
-			2024 &copy; <strong><a target="_blank" alt="Kyodai" href="/">Kyodai</a></strong>
-			- &copy; <strong><a target="_blank" alt="Paiheme studio" href="https://paihemestudio.com">Paiheme Studio</a></strong>
-			- <strong><a target="_blank" alt="Kyodai" href="/mentions-legales">Mentions légales</a></strong>
-			- 
-			<strong><a target="_blank" alt="Kyodai" href="/mentions-legales">RGPD</a></strong>
-			- Site web par <strong><a target="_blank" href="https://hatsumei.fr" alt="Hatsumei" >Hatsumei</a></strong>
-		</p>
-	</div>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
+		</main><!-- /#main-content -->
+		<footer role="contentinfo">
+			<span class="logo_footer"><a href="/" alt="Retour à l'accueil"> <img src="<?php echo get_template_directory_uri(); ?>/dist/image/LOGO_KYO.png" alt="Kyodai Logo" class="footer-logo"></a></span>
+			<p>
+				<span class="legal">2024 &copy;
+					<strong><a target="_blank" alt="Kyodai" href="/">Kyodai</a></strong>
+					- &copy; <strong><a target="_blank" alt="Paiheme studio" href="https://paihemestudio.com">Paiheme Studio</a></strong>
+					- <strong><a target="_blank" alt="Kyodai" href="/mentions-legales">Mentions légales</a></strong>
+					-
+					<strong><a target="_blank" alt="Kyodai" href="/mentions-legales">RGPD</a></strong>
+					- Site web par <strong><a target="_blank" href="https://le-tengu.fr" alt="Tanguy le Tengu">TYLT</a></strong>
+				</span>
+				<span class="instagram">
+					<a href="https://www.instagram.com/kyodaiprint" target="_blank" alt="Instagram Kyodai"><i class="icon"></i>@kyodai.print</a>
+				</span>
+				<span class="mail">
+					<a href="mailto:pierre@kyodai.shop" target="_blank" alt="Linkedin Kyodai"><i class="icon"></i>pierre@kyodai.shop</a>
+				</span>
+			</p>
+		</footer>
+		<?php wp_footer(); ?>
+		</body>
+
+		</html>
