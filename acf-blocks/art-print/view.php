6ca71b5740023934aca03588c1e2d103750fbd28
Tanguy Pinel <tanguypinel@gmail.com>
2024-10-15 14:34:27 +0200

    ok need video

--- acf-blocks/art-print/view.php
+++ acf-blocks/art-print/view.php
@@ -12,13 +12,18 @@
             </div>
         <?php endif; ?>
         <?php if (wp_is_mobile()) : ?>
-            <?php foreach ($images_mobile as $image) : extract($image)?>
+            <?php foreach ($images_mobile as $image) : extract($image) ?>
                 <div class="video-container mobile" data-aos="fade-up">
-                    <?php $url = wp_get_attachment_url( $image_mobile ); ?>
+                    <?php $url = wp_get_attachment_url($image_mobile); ?>
+                    <?php $url_alt = wp_get_attachment_url($image_mobile_alt); ?>
                     <video loop autoplay muted>
-                        <source src="<?php echo esc_html($url); ?>" type="video/webm">
-                        Your browser does not support the video tag.
+                        <?php if ($url_alt && strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== false && strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') === false) : ?>    <source src="<?php echo esc_html($url_alt); ?>" type="video/mov">
+                            Your browser does not support the video tag.
+                        <?php else : ?>
+                            <source src="<?php echo esc_html($url); ?>" type="video/webm">
+                        <?php endif; ?>
                     </video>
+
                 </div>
             <?php endforeach; ?>
         <?php endif; ?>
