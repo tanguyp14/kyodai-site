5a563748fd40ada5dcf1acfccd60bd85c01d049e
Tanguy Pinel <tanguypinel@gmail.com>
2024-10-18 14:34:23 +0200

    Update styles in acf-blocks/alone-button, acf-blocks/call-formulaire, acf-blocks/contact-banniere, and acf-blocks/formulaire

--- acf-blocks/marquee/assets/js/view.js
+++ acf-blocks/marquee/assets/js/view.js
@@ -6,11 +6,11 @@
 
 				var marqueeTextElement = $('#' + id + ' .marquee_text');
 				var marqueeText = marqueeTextElement.html();
-		
+				
 				// Créer un conteneur pour le texte défilant
 				var marqueeContainer = $('<div class="' + id + ' marquee_container"></div>');
 				var marqueeContent = $('<div class="' + id + ' marquee_content" style="animation-duration:' + speed + 's"></div>');
-		
+				
 				// Ajouter initialement le texte une première fois
 				marqueeContent.append(marqueeText + ' ');
 		
@@ -20,19 +20,27 @@
 		
 				// Calculer la nouvelle largeur après avoir ajouté une première itération
 				var marqueeTextWidth = marqueeContent.outerWidth(true);
-				console.log('Marquee Text Width after adding content:', marqueeTextWidth);
 		
 				// Calculer le nombre de répétitions
-				var repeatCount = Math.ceil(($(window).width() / marqueeTextWidth) * 2);
-				console.log('Repeat Count:', repeatCount);
+				var repeatCount = Math.ceil(($(window).width() / marqueeTextWidth));
 		
 				// Ajouter des itérations supplémentaires
 				for (var i = 1; i < repeatCount; i++) {
 					marqueeContent.append(marqueeText + ' ');
 				}
-		
-				// Après avoir ajouté toutes les répétitions, vérifier la largeur
-				console.log('Final Marquee Content Width:', marqueeContent.outerWidth(true));
+				var inside = $('#' + id + ' .marquee_content .inside').width();
+
+				// Ajouter l'animation de translation
+				marqueeContent.css({
+					'display': 'flex',
+					'white-space': 'nowrap',
+					'animation': 'marqueeAnimation ' + speed + 's linear infinite'
+				});
+
+				// Ajouter les styles d'animation dans une balise <style>
+				var style = $('<style>@keyframes marqueeAnimation { 0% { transform: translateX(0); } 100% { transform: translateX(-' + (inside + 20) + 'px); } }</style>');
+				$('head').append(style);
+
 			});
 		});
 })(jQuery);
