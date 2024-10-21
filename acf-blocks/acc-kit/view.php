bac25dbfe9580d4b6175583bb6d1d75d26e9b226
Tanguy Pinel <tanguypinel@gmail.com>
2024-10-21 09:35:47 +0200

    update

--- acf-blocks/acc-kit/view.php
+++ acf-blocks/acc-kit/view.php
@@ -2,7 +2,7 @@
     $fields = get_fields();
     extract($fields);
     ?>
-    <section class="kit_papier">
+    <section id="kit" class="kit_papier">
         <div class="image_pack" data-aos="<?= wp_is_mobile() ? 'fade-right' : 'right' ?>" data-aos-easing="ease-in-out" data-aos-delay="300">
             <?php if ($image) : ?>
                 <?php echo wp_get_attachment_image($image, 'full'); ?>
@@ -11,8 +11,12 @@
         <div class="text_pack">
             <h2><?php echo $titre; ?></h2>
             <p class="sous-titre"><?php echo $sous_titre; ?></p>
-            <p class="z-t"><?php echo $zone_de_texte; ?></p>
-            <p class="z-t-t"><?php echo $zone_de_gras; ?></p>
-            <span class="link red"><a href="<?php echo $lien; ?>" alt="Vers la demande de Kit papier" class="cta">Choper mon kit</a></span>
+            <?php if ($formulaire_ou_lien == 0) : ?>
+                    <p class="z-t"><?php echo $zone_de_texte; ?></p>
+                    <p class="z-t-t"><?php echo $zone_de_gras; ?></p>
+                    <span class="link red"><a href="<?php echo $lien; ?>" alt="Vers la demande de Kit papier" class="cta">Choper mon kit</a></span>
+            <?php else : ?>
+                <?php echo do_shortcode($shortcode_formulaire); ?>
+            <?php endif; ?>
         </div>
     </section>
--- acf-blocks/marquee/assets/js/view.js
+++ acf-blocks/marquee/assets/js/view.js
@@ -1,46 +1,48 @@
 (function ($) {
-		$(document).ready(function () {
-			$('.marquee').each(function () {
-				var id = $(this).attr('id');
-				var speed = $(this).attr('data-speed');
+	$(document).ready(function () {
+		$('.marquee').each(function () {
+			var id = $(this).attr('id');
+			var speed = $(this).attr('data-speed');
 
-				var marqueeTextElement = $('#' + id + ' .marquee_text');
-				var marqueeText = marqueeTextElement.html();
-				
-				// Créer un conteneur pour le texte défilant
-				var marqueeContainer = $('<div class="' + id + ' marquee_container"></div>');
-				var marqueeContent = $('<div class="' + id + ' marquee_content" style="animation-duration:' + speed + 's"></div>');
-				
-				// Ajouter initialement le texte une première fois
+			var marqueeTextElement = $('#' + id + ' .marquee_text');
+			var marqueeText = marqueeTextElement.html();
+
+			// Créer un conteneur pour le texte défilant
+			var marqueeContainer = $('<div class="' + id + ' marquee_container"></div>');
+			var marqueeContent = $('<div class="' + id + ' marquee_content" style="animation-duration:' + speed + 's"></div>');
+
+			// Ajouter initialement le texte une première fois
+			marqueeContent.append(marqueeText + ' ');
+
+			// Ajouter le contenu au conteneur avant d'autres calculs
+			marqueeContainer.append(marqueeContent);
+			marqueeTextElement.replaceWith(marqueeContainer);
+
+			// Calculer la nouvelle largeur après avoir ajouté une première itération
+			var marqueeTextWidth = marqueeContent.outerWidth(true);
+
+			// Calculer le nombre de répétitions
+			var repeatCount = Math.ceil(($(window).width() / marqueeTextWidth) + 1);
+
+			// Ajouter des itérations supplémentaires
+			for (var i = 1; i < repeatCount; i++) {
 				marqueeContent.append(marqueeText + ' ');
-		
-				// Ajouter le contenu au conteneur avant d'autres calculs
-				marqueeContainer.append(marqueeContent);
-				marqueeTextElement.replaceWith(marqueeContainer);
-		
-				// Calculer la nouvelle largeur après avoir ajouté une première itération
-				var marqueeTextWidth = marqueeContent.outerWidth(true);
-		
-				// Calculer le nombre de répétitions
-				var repeatCount = Math.ceil(($(window).width() / marqueeTextWidth));
-		
-				// Ajouter des itérations supplémentaires
-				for (var i = 1; i < repeatCount; i++) {
-					marqueeContent.append(marqueeText + ' ');
-				}
-				var inside = $('#' + id + ' .marquee_content .inside').width();
+			}
 
-				// Ajouter l'animation de translation
-				marqueeContent.css({
-					'display': 'flex',
-					'white-space': 'nowrap',
-					'animation': 'marqueeAnimation ' + speed + 's linear infinite'
-				});
+			// Calculer la largeur de .inside pour cet élément spécifique
+			var insideWidth = $('#' + id + ' .inside').width();
 
-				// Ajouter les styles d'animation dans une balise <style>
-				var style = $('<style>@keyframes marqueeAnimation { 0% { transform: translateX(0); } 100% { transform: translateX(-' + (inside + 20) + 'px); } }</style>');
-				$('head').append(style);
-
+			// Ajouter l'animation de translation
+			marqueeContent.css({
+				'display': 'flex',
+				'white-space': 'nowrap',
+				'animation': 'marqueeAnimation_' + id + ' ' + speed + 's linear infinite'
 			});
+
+			// Ajouter les styles d'animation dans une balise <style>
+			var style = $('<style>@keyframes marqueeAnimation_' + id + ' { 0% { transform: translateX(0); } 100% { transform: translateX(-' + (insideWidth + 20) + 'px); } }</style>');
+			$('head').append(style);
+
 		});
+	});
 })(jQuery);
